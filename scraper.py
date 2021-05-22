8a487d791cdc0812318f4c8d477ff54f1fbf3a2c
Rafasmafa <nrafanelli@gmail.com>
2021-05-22 11:36:16 -0400

    Added comments and type hints

--- scraper.py
+++ scraper.py
@@ -3,14 +3,29 @@ import re
 import os
 import smtplib
 
-from datetime import date, datetime
+from datetime import datetime
 from selenium import webdriver
 from drivers.driver_path import chrome_driver_path
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 class NjDmvScraper:
-    def __init__(self, cities, search_months):
+    '''
+    This class scapes the NJ dmv appointment website for OUT OF STATE
+    LICENSE TRANSERS ONLY. To check for other appointments change self.base_url
+
+    Parameters:
+        cities: list of cities to check appointents for. (ex:['newark', 'wayne'] )
+        serach_month: list of months to check for open appointments. (ex:['May', 'June'] )
+
+    Environment Variables:
+        PHONE_NUMBER: a valid verizon wireless phone number to send texts to.
+            must be version because verizon "vtext" functionality is used.
+        GMAIL: gmail.com email address
+        GMAIL_PASSWORD: password for gmail email address
+
+    '''
+    def __init__(self, cities:list[str], search_months:list[str]):
         self.cities = cities
         self.search_months = search_months
         self.base_url = 'https://telegov.njportal.com/njmvc/AppointmentWizard/7'
@@ -22,14 +37,25 @@ class NjDmvScraper:
             self.found_appts[city] = []
 
     def run(self):
+        '''
+        Check DMV appointments every minute until the program is killed
+        '''
         while True:
             try:
                 self.check_open_appointments(quit_when_done=False)
-                time.sleep(5)
+                time.sleep(60)
             except Exception:
                 self.driver.quit()
 
-    def check_open_appointments(self, quit_when_done=True):
+    def check_open_appointments(self, quit_when_done:bool=True):
+        '''
+        Checks base url for open appointments within the given cities and months
+        that were given in the constructor.
+
+        Parameters:
+            quit_when_done: close chrome driver when execution is finished
+
+        '''
         for city in self.cities:
             self.driver.get(self.base_url)
             print (f'CHECKING APPOINTMENTS IN {city.title()}', flush=True)
@@ -54,22 +80,58 @@ class NjDmvScraper:
         if quit_when_done:
             self.driver.quit()
 
-    def _is_valid_appointment(self, city1, city2, appt_dt):
+    def _is_valid_appointment(self, city1: str, city2: str, appt_dt: datetime):
+        '''
+        Helper function to determine if the cities match, the appointment
+        date is in the search_months and if we have already found the appointment
+
+        Parameters:
+            city1: a city name.
+            city2: a city name.
+            appt_dt: appointment datetime object
+
+        Return: bool
+
+        '''
         return (city1 == city2 and \
                 appt_dt.strftime("%B") in self.search_months and \
                 appt_dt not in self.found_appts[city1])
 
-    def get_next_appt_dt(self, city_node_text):
+    def get_next_appt_dt(self, city_node_text:str):
+        '''
+        Parse out date from city node text using %m/%d/%Y %I:%M %p"
+        pattern and convert it to a datetie object
+
+        Parameters:
+            city_node_text: text from the city node html
+
+        Return: datetime
+
+        '''
         regex = r"(?<=Next Available:\s).+(?=\sMAKE APPOINTMENT)"
         dt_string = re.search(regex, city_node_text).group(0)
         return datetime.strptime(dt_string, "%m/%d/%Y %I:%M %p")
 
-    def send_message(self, city_name, date, link):
+    def send_message(self, city_name:str, date:str, link:str):
+        '''
+        Sends a text message to the verizon phone number given
+        in the environment variable PHONE_NUMBER by sending an
+        email from the gmail account specified using the
+        environment varibles GMAIL and GMAil_PASSWORD.
+
+        Note: you can only send emails to a phone number if its a
+        verizon phone number.
+
+         Parameters:
+            city_name: city name of the appointment.
+            date: date of the appointment.
+            link: link to the appointment.
+        '''
         msg = MIMEMultipart()
         msg['From'] = os.environ.get('GMAIL')
         msg['To'] = self.phone_email
         msg['Subject'] = 'OPEN DMV APPOINTMENT'
-        body = (f"Open appointmen in {city_name} on {date} \n {link}")
+        body = (f"Open appointment in {city_name} on {date} \n {link}")
         msg.attach(MIMEText(body, 'html'))
 
         # creates SMTP session
@@ -91,10 +153,14 @@ class NjDmvScraper:
         s.quit()
 
     def _setup_chromedriver(self):
+        '''
+        Setup and init chrome driver.
+        '''
         options = webdriver.ChromeOptions()
         options.add_argument('--headless')
         return webdriver.Chrome(chrome_driver_path, options=options)
 
-cities = ['vineland', 'newark']
-months = ['May', 'June']
-NjDmvScraper(cities, months).run()
+if __name__ == "__main__":
+    cities = ['newark', 'wayne']
+    months = ['May', 'June']
+    NjDmvScraper(cities, months).run()
